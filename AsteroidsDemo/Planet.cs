b307ce6764a08fdfdcf8b5bdd79c240dc2bc9347
Александр Гусев <scorp.predateur@gmail.com>
2020-01-28 20:39:09 +0300

    Подцепил картинки в классе Planet.

--- AsteroidsDemo/Planet.cs
+++ AsteroidsDemo/Planet.cs
@@ -13,7 +13,7 @@ namespace AsteroidsDemo
         /// <param name="size"> Размер. </param>
         public Planet(Vector2 pos, Vector2 dir, Size size) : base(pos, dir, size)
         {
-
+            Sprite = Sprites[Game.R.Next(Sprites.Length)];
         }
 
         /// <summary>
@@ -21,7 +21,7 @@ namespace AsteroidsDemo
         /// </summary>
         public override void Draw()
         {
-            Game.Buffer.Graphics.FillEllipse(Brushes.Wheat, Pos.X, Pos.Y, Size.Width, Size.Height);
+            Game.Buffer.Graphics.DrawImage(Sprite, Pos.X, Pos.Y, Size.Width, Size.Height);
         }
 
         /// <summary>
@@ -35,6 +35,27 @@ namespace AsteroidsDemo
 
             Pos.X = Game.Width + Size.Width;
             Pos.Y = Game.R.Next(Game.Height - Size.Height);
+        }
+
+        /// <summary>
+        /// Рисуемый объект Image объекта Planet.
+        /// </summary>
+        public Bitmap Sprite { get; set; }
+
+        private static readonly Bitmap[] Sprites;
+
+        static Planet()
+        {
+            Sprites = new[]
+            {
+                Resources.Exoplanet,
+                Resources.Exoplanet2,
+                Resources.Exoplanet3,
+                Resources.RedGiant,
+                Resources.IceGiant,
+                Resources.GasGiant,
+                Resources.Sun
+            };
         }
     }
 }
