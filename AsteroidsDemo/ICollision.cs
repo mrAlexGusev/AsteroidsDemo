59a82b5d3616e193dd5b3797bf19be606e342fed
Александр Гусев <scorp.predateur@gmail.com>
2020-02-01 16:52:06 +0300

    Добавил интерфейс столкновения объектов.

---
+++ AsteroidsDemo/ICollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace AsteroidsDemo
+{
+    /// <summary>
+    /// Интерфейс объектов, которые могут сталкиваться.
+    /// </summary>
+    interface ICollision
+    {
+        Rectangle Rect { get; }
+
+        bool Collision(ICollision obj);
+    }
+}
