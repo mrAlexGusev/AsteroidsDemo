952026c8105f89d9008d415612034abde4e713b9
Александр Гусев <scorp.predateur@gmail.com>
2020-02-02 11:13:30 +0300

    Добавил класс ошибок игры. Реализовал одни из базовых конструкторов.

---
+++ AsteroidsDemo/GameObjectException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsteroidsDemo
+{
+    /// <summary>
+    /// Класс собственных ошибок в игре. Требуется по заданию.
+    /// </summary>
+    class GameObjectException : Exception
+    {
+        public GameObjectException(string message) : base(message) { }
+    }
+}
