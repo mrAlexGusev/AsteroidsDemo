54d00a1906e68aec18c6c38c7b75ac0b2734abee
Александр Гусев <scorp.predateur@gmail.com>
2020-02-03 09:51:37 +0300

    Добавил класс обработчика нажатия клавиш.

---
+++ AsteroidsDemo/KeysHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AsteroidsDemo
+{
+    /// <summary>
+    /// Класс обработки нажатых клавиш.
+    /// </summary>
+    static class KeysHandler
+    {
+        /// <summary>
+        /// Список нажатых клавиш.
+        /// </summary>
+        private static readonly List<Keys> _keys;
+
+        /// <summary>
+        /// Инициализация.
+        /// </summary>
+        static KeysHandler()
+        {
+            _keys = new List<Keys>();
+        }
+
+        /// <summary>
+        /// Обработчик события нажатия клавиш.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!IsPressed(e.KeyCode)) _keys.Add(e.KeyCode);
+        }
+
+        /// <summary>
+        /// Обработчик события отжатия клавиш.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void KeyUp(object sender, KeyEventArgs e)
+        {
+            _keys.Remove(e.KeyCode);
+        }
+
+        /// <summary>
+        /// Проверка нажатия клавиши.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static bool IsPressed(Keys key)
+        {
+            return _keys.Contains(key);
+        }
+    }
+}
